2e846e996204c5f9045b1394d7d186c8e5706207
KamrulHasan05 <mdkamrulhasan.du@gmail.com>
2024-11-26 21:45:29 +0600

    some basic console

---
+++ JSONE.js
@@ -0,0 +1,15 @@
+console.log(10);
+
+let isMarried = true;
+
+console.log(isMarried);
+
+console.log(typeof isMarried);
+
+let age;
+
+let food = " ";
+
+console.log(typeof age);
+
+console.log(typeof food);
